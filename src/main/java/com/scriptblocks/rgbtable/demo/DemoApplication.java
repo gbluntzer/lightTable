f45b78cfac2716bde07c99ab02602dc1dbe4a32a
gbluntzer <gbluntzer@gmail.com>
2018-03-24 16:18:48 -0500

    updated gradle config

--- src/main/java/com/scriptblocks/rgbtable/demo/DemoApplication.java
+++ src/main/java/com/scriptblocks/rgbtable/demo/DemoApplication.java
@@ -3,10 +3,35 @@ package com.scriptblocks.rgbtable.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 @SpringBootApplication
 public class DemoApplication {
 
+
+	public DemoApplication() {
+		init();
+	}
+
+	public void init(){
+		try {
+			Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+			while(networkInterfaces.hasMoreElements()){
+				NetworkInterface ni = networkInterfaces.nextElement();
+				InetAddress inetAddress = ni.getInetAddresses().nextElement();
+				String ipAddress = inetAddress.getHostAddress();
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+
 	}
 }
